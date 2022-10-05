13d1d130fd612b7897ae2fb02d4441a07969a75b
waveofdandelion <www.dandelion.io@gmail.com>
2022-10-05 04:11:33 +0200

    chore: add task for server

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const { series, parallel } = require("gulp");
 const styles = require("./tasks/Style");
 const scripts = require("./tasks/Script");
+const watch = require("./tasks/Server");
 
-exports.default = series(series(styles.styles), scripts.scripts);
+exports.default = series(
+    series(styles.styles, scripts.scripts),
+    parallel(watch.sync, watch.startWatch)
+);
