6daa020404d93319311de8f21069d15d539d66c0
waveofdandelion <www.dandelion.io@gmail.com>
2022-10-07 23:28:25 +0200

    fix(tasks): fix clean task

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,6 @@ const html = require("./tasks/Html");
 const assets = require("./tasks/Public");
 const clean = require("./tasks/Clean");
 
-// You can change it from helpers/Option.js
-exports.devInit = series(
-    series(
-        this.deepCleanSrc || this.cleanSrc,
-        this.convertFonts || this.rebaseFonts,
-        this.resizeImages || this.rebaseImages,
-        styles.styles,
-        scripts.scripts
-    )
-);
-
 // All image transformations, the second task just for rebase images
 if (options.imageResize) {
     exports.resizeImages = series(
@@ -40,28 +29,28 @@ if (options.imageResize) {
     exports.rebaseImages = series(images.images, images.cachemin);
 }
 
-// All font transformations, the second task just for rebase fonts
+// // All font transformations, the second task just for rebase fonts
 if (options.fontGeneration) {
     exports.convertFonts = series(
         fonts.transform,
-        // fonts.transformToWoff2,
+        fonts.transformToWoff2,
         fonts.ttfRebase
     );
 } else {
     exports.rebaseFonts = series(fonts.transform, fonts.ttfRebase);
 }
 
-// HTML minification task
+// // HTML minification task
 exports.htmlMin = series(html.html);
 
-// Clean all src or only styles and js files
+// // Clean all src or only styles and js files
 if (options.deepCleanSrc) {
     exports.deepCleanSrc = series(clean.deepCleanSrc);
 } else {
     exports.cleanSrc = clean.cleanSrc;
 }
 
-// Build Task
+// // Build Task
 exports.build = series(
     clean.cleanPublic,
     styles.styles,
@@ -78,7 +67,18 @@ exports.build = series(
 );
 
 // Default main task for development you should to use it after devInit
-exports.lol = series(
+exports.dev = series(
     series(styles.styles, scripts.scripts),
     parallel(watch.sync, watch.startWatch)
+);
+
+// You can change it from helpers/Option.js
+exports.devInit = series(
+    series(
+        this.deepCleanSrc || this.cleanSrc,
+        this.convertFonts || this.rebaseFonts,
+        this.resizeImages || this.rebaseImages,
+        styles.styles,
+        scripts.scripts
+    )
 );
